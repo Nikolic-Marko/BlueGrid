abb52453a7f081dcc6d0fdfd7af926544715442c
Nikolic-Marko <marko@yoordi.com>
2024-06-18 11:11:04 +0200

    remove node_modules

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import fetch from "node-fetch";
 import { transformData } from "./utils";
 
 interface ApiResponse {
@@ -9,8 +9,12 @@ const FILES_API_URL = "https://rest-test-eight.vercel.app/api/test";
 
 export const fetchDataAndTransform = async (): Promise<Record<string, any[]>> => {
     try {
-        const response = await axios.get<ApiResponse>(FILES_API_URL);
-        const urls = response.data.items;
+        const response = await fetch(FILES_API_URL);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data: ApiResponse = (await response.json()) as ApiResponse;
+        const urls = data.items;
         return transformData(urls);
     } catch (error) {
         console.error("Error fetching data from API:", error);
